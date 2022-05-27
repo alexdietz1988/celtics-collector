48f1a458edb2e76f3b0c8bb24140e8a4834d369c
alexdietz1988 <alex.dietz@gmail.com>
2022-05-27 15:12:19 -0400

    Create placeholder templates for base, home, celtics-detail, and celtics-list

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.Home.as_view(), name="home"),
+    path('celtics-list', views.CelticsList.as_view(), name="celtics-list"),
+    path('celtics-detail', views.CelticsDetail.as_view(), name="celtics-detail")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html"
+
+class CelticsList(TemplateView):
+    template_name = "celtics-list.html"
+
+class CelticsDetail(TemplateView):
+    template_name = "celtics-detail.html"
