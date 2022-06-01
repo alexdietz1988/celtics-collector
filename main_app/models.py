bea2cd8b5de40747980c585ab0f782feaa0662e9
alexdietz1988 <alex.dietz@gmail.com>
2022-06-01 09:30:47 -0400

    Simplify player model

--- main_app/models.py
+++ main_app/models.py
@@ -8,12 +8,6 @@ class Player(models.Model):
     last_name = models.CharField(max_length=100)
     img = models.CharField(max_length=250)
     number = models.IntegerField(default=-1)
-    position = models.CharField(max_length=100)
-    age = models.IntegerField(default=0)
-    height_in_inches = models.IntegerField(default=0)
-    weight_in_lbs = models.IntegerField(default=0)
-    college = models.CharField(max_length=250)
-    salary_in_dollars = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
